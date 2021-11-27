6fbd33a210946ca80e5d6cb4d988d4d0f65f2c34
zairoff <marufzoiroff@gmail.com>
2021-11-27 09:59:22 +0500

    added console app to demonstaration

---
+++ Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new DoublyLinkedList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
